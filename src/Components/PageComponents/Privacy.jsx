59b84506c9381e9aa7341d1821541f79a5689d34
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-10-01 07:34:27 -0400

    working on the privacy page

---
+++ src/Components/PageComponents/Privacy.jsx
@@ -0,0 +1,30 @@
+import React, {Compnent} from 'react';
+
+class PrivacyPage extends Component{
+  render(){
+    return(
+      <div className='container'>
+        <h1>Privacy & Security Policy</h1>
+        <p>
+          We would like to take this opportunity to both thank you for visiting our website and to share with you the importance <span className='title-name'>Canine Liability</span> places upon protecting your private information.
+        </p>
+        <h2>Collection of Information:</h2>
+        <p>
+          While you are visiting our website, we may or may not collect personal/confidential information- written, in-person, telephone, facsimile, electronically and online- that may assist us in preparing a quote for you and in helping us to negotiate with your carriers on your behalf. The personal information we collect may include your name, address, policy, or claim information, financial, or other information as may be required by our underwriters.
+        </p>
+        <p>
+          We also collect personal information about you from the following sources:
+        </p>
+        <ul>
+          <li>Applications or other forms such as claims forms or underwriting questionnaires that you submit to us and others;</li>
+          <li>Information about your transactions with us, our affiliates or others; and</li>
+          <li>Third party information from public sources such as the internet or governmental agencies.</li>
+        </ul>
+        <h2>Disclosure of Information:</h2>
+        <p>
+          
+        </p>
+      </div>
+    )
+  }
+}
